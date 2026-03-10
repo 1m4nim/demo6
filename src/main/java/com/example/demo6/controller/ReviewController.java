4dde5ac2940a6760adb7b06d8a0b68d3f9f41e4b
minami <minami@MacBook-Air.local>
2026-03-10 11:57:16 +0900

    HTML書いた

--- src/main/java/com/example/demo6/controller/ReviewController.java
+++ src/main/java/com/example/demo6/controller/ReviewController.java
@@ -2,7 +2,7 @@ package com.example.demo6.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping; // 追加が必要
+import org.springframework.web.bind.annotation.GetMapping; // これが必要
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.demo6.form.ReviewRegistForm;
@@ -10,27 +10,28 @@ import com.example.demo6.form.ReviewRegistForm;
 @Controller
 public class ReviewController {
 
+    // 入力画面を表示（ブラウザでURLを叩くのはGETリクエスト）
     @GetMapping("/show-review-form")
     public String showReviewForm(@ModelAttribute ReviewRegistForm form) {
         return "regist-review";
     }
 
-
+    // 確認画面から「戻る」ボタンを押した時用
     @PostMapping("/show-review-form-ret")
     public String showReviewFormRet(@ModelAttribute ReviewRegistForm form) {
         return "regist-review";
     }
 
+    // 入力画面から「確認」ボタンを押した時
     @PostMapping("/regist-review")
-    public String registReview(@ModelAttribute ReviewRegistForm form, Model model) {
-        // 確認画面で値を表示するために Model に追加
+    public String registacReview(@ModelAttribute ReviewRegistForm form, Model model) {
         model.addAttribute("reviewRegistForm", form);
         return "confirm-regist-review";
     }
 
+    // 確認画面から「登録」ボタンを押した時
     @PostMapping("/confirm-regist-review")
     public String confirmRegistReview(@ModelAttribute ReviewRegistForm form, Model model) {
-        // @ModelAttribute を付けることで、完了画面でもデータを使えるようにする
         model.addAttribute("msg", "レビュー登録が完了しました");
         return "complete-regist-review";
     }
